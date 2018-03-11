1f4e34f5ee73f305832c944f1f0d401c2b0b52eb
Kirsanov Alexander <kahigh@yandex.ru>
2018-03-11 14:54:10 +0300

    add input name & output msg

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("How are you??");
+            
+            Console.WriteLine("How are your name?");
+            string str = Console.ReadLine();
+            
+            Console.WriteLine("Hello {0}!", str);
+
             // новые изменения
             Console.ReadLine();
         }
